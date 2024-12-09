381373406a2568300d8e1806b09b6b67de315fbb
fadyyassa <fadysamirh@gmail.com>
2024-12-09 07:05:46 +0200

    type fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { Theme } from "@/styles/theme/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { AppContextProvider } from "@/contexts/app-context/app-context";
-import { SnackbarProvider } from "@/contexts/snackbar-context/snackbar-context";
 import { AppNotifications } from "@/components/common/app-notifications/app-notifications";
 
 const clientSideEmotionCache = createEmotionCache();
@@ -26,9 +25,7 @@ export default function RootLayout({
           <CssBaseline />
           <body>
             <AppNotifications />
-            <SnackbarProvider>
-              <AppContextProvider>{children}</AppContextProvider>
-            </SnackbarProvider>
+            <AppContextProvider>{children}</AppContextProvider>
           </body>
         </ThemeProvider>
       </CacheProvider>
--- src/contexts/snackbar-context/snackbar-context.type.ts
+++
@@ -1,7 +0,0 @@
-export type SnackbarContextType = {
-  showSnackbar: (message: string, severity: string) => void;
-};
-
-export const SnackbarContextDefaultValue: SnackbarContextType = {
-  showSnackbar: () => {},
-};
